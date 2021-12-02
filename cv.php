d36d4c32f1bc02dba1a9c0e69bd653cb28213e6a
Sencer Yazici <sencer.yazici@picknik.ai>
2021-12-03 02:10:26 +0300

    add cv

---
+++ cv.php
@@ -0,0 +1,10 @@
+<?php
+    $file = "https://github.com/senceryazici/cv/releases/latest/download/main.pdf"
+
+    header("Content-Description: File Transfer"); 
+    header("Content-Type: application/octet-stream"); 
+    header("Content-Disposition: attachment; filename=\"". basename($file) ."\""); 
+    
+    readfile ($file);
+    exit(); 
+?>
